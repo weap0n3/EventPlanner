67da24bd55b20fa013d7e537117f6ba765ce001a
weap0n3 <weap0n1823@gmail.com>
2025-05-15 10:31:32 +0100

    add CommunityToolkit

--- EventPlanner.Gui/MauiProgram.cs
+++ EventPlanner.Gui/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace EventPlanner.Gui
 {
@@ -9,6 +10,7 @@ namespace EventPlanner.Gui
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
