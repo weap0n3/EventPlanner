56fae037a30bfcb6a8c02c1d1a854d09d87c9d95
weap0n3 <weap0n1823@gmail.com>
2025-05-17 11:18:21 +0100

    add Singleton

--- EventPlanner.Gui/MauiProgram.cs
+++ EventPlanner.Gui/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Maui;
+using EventPlanner.Core.ViewModels;
 using Microsoft.Extensions.Logging;
 
 namespace EventPlanner.Gui
@@ -17,6 +18,9 @@ namespace EventPlanner.Gui
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("Raleway-VariableFont_wght.ttf", "Raleway");
                 });
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
+
 
 #if DEBUG
     		builder.Logging.AddDebug();
