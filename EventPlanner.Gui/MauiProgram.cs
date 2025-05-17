4a2d247059e6375a2e1d5083e097883a84e251ba
weap0n3 <weap0n1823@gmail.com>
2025-05-17 10:27:39 +0100

    add new font

--- EventPlanner.Gui/MauiProgram.cs
+++ EventPlanner.Gui/MauiProgram.cs
@@ -15,6 +15,7 @@ namespace EventPlanner.Gui
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Raleway-VariableFont_wght.ttf", "Raleway");
                 });
 
 #if DEBUG
