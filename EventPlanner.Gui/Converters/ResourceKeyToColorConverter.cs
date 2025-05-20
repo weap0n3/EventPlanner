f26a3c539e6ebf75a56a1c9eb1958ce439ea0dd2
weap0n3 <weap0n1823@gmail.com>
2025-05-20 09:02:09 +0100

    create new Converter

---
+++ EventPlanner.Gui/Converters/ResourceKeyToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Gui.Converters;
+
+public class ResourceKeyToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string key && Application.Current.Resources.TryGetValue(key, out var colorObj))
+        {
+            return colorObj;
+        }
+
+        return Colors.Transparent; // fallback color
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException(); // Not needed for one-way binding
+    }
+}
