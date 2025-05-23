8f8341cccf4d87614abf7695d76b95dfc90915b9
weap0n3 <weap0n1823@gmail.com>
2025-05-23 12:38:51 +0100

    create database interface

---
+++ EventPlanner.Data/Services/IDatabase.cs
@@ -0,0 +1,14 @@
+﻿using EventPlanner.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Data.Services;
+
+public interface IDatabase
+{
+    bool AddEvent(Event e);
+    List<Event> GetEvents();
+}
