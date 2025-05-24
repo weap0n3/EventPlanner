3653b8b124c2782f083c7fb0570884f9ee640085
weap0n3 <weap0n1823@gmail.com>
2025-05-24 12:18:59 +0100

    create API

---
+++ EventPlanner.Data/Services/DBRepository.cs
@@ -0,0 +1,41 @@
+﻿using EventPlanner.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Data.Services;
+
+public class DBRepository : IDatabase
+{
+    private string _dbPath;
+
+    public DBRepository(string dbPath)
+    {
+        this._dbPath = dbPath;
+    }
+    public bool AddEvent(Event e)
+    {
+        try
+        {
+            var context = new EventContext(_dbPath);
+            context.Add(e);
+            context.SaveChanges();
+            return true;
+        } 
+        catch(Exception err) 
+        {
+            System.Diagnostics.Debug.WriteLine(err);
+            return false;
+        }
+    }
+
+    public List<Event> GetEvents()
+    {
+        var context = new EventContext(_dbPath);
+        var events = (from ev in context.Events
+                     select ev).ToList();
+        return events;
+    }
+}
