3ee1ee07ad4c49e6985a074186c1e546c1ee9f3e
weap0n3 <weap0n1823@gmail.com>
2025-05-26 15:02:41 +0100

    create a ViewModel

---
+++ EventPlanner.Core/ViewModels/AllEventsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EventPlanner.Data.Models;
+using EventPlanner.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Core.ViewModels;
+
+public partial class AllEventsViewModel : ObservableObject
+{
+    IDatabase _db;
+    public AllEventsViewModel(IDatabase db)
+    {
+        this._db = db;
+    }
+
+    [ObservableProperty]
+    private ObservableCollection<Event> _events;
+
+    [ObservableProperty]
+    private string _title;
+
+    [ObservableProperty]
+    private string _description;
+
+    [ObservableProperty]
+    private DateTime _date;
+
+    private bool IsLoaded = true;
+
+    [RelayCommand]
+    void Load()
+    {
+        if (IsLoaded)
+        {
+            var events = _db.GetEvents();
+            foreach (var ev in events)
+            {
+                _events.Add(ev);
+            }
+            IsLoaded = !IsLoaded;
+        }
+    }
+}
