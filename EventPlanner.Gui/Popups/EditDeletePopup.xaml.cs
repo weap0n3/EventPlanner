ace6825accc4e83e8865d3cc522fa928a5a8fa74
weap0n3 <weap0n1823@gmail.com>
2025-06-01 22:27:32 +0100

    updated Popup

--- EventPlanner.Gui/Popups/EditDeletePopup.xaml.cs
+++ EventPlanner.Gui/Popups/EditDeletePopup.xaml.cs
@@ -1,11 +1,31 @@
 using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Mvvm.Messaging;
+using EventPlanner.Core.Messages;
+using EventPlanner.Data.Models;
+using EventPlanner.Data.Services;
 
 namespace EventPlanner.Gui.Popups;
 
 public partial class EditDeletePopup : Popup
 {
-	public EditDeletePopup()
-	{
-		InitializeComponent();
-	}
+    private readonly Event _event;
+    private EventService _eventService;
+
+    public EditDeletePopup(Event ev, EventService eventService)
+    {
+        InitializeComponent();
+        _event = ev;
+        this._eventService = eventService;
+    }
+
+    private void OnDeleteClicked(object sender, EventArgs e)
+    {
+        // Option 1: Use EventService directly (if injected)
+        var result = _eventService.Delete(_event);
+        if (result)
+        {
+            WeakReferenceMessenger.Default.Send(new DeleteEventMessage(_event));
+        }
+        Close();
+    }
 }
