54edf8df17372774d062dcad32f4a4424b5e705a
weap0n3 <weap0n1823@gmail.com>
2025-06-01 22:29:28 +0100

    create new Message

---
+++ EventPlanner.Core/Messages/DeleteEventMessage.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using EventPlanner.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Core.Messages;
+
+public class DeleteEventMessage : ValueChangedMessage<Event>
+{
+    public DeleteEventMessage(Event value) : base(value)
+    {
+    }
+}
