b034937601d7027d3c0e5958dd07f36b4a85480b
weap0n3 <weap0n1823@gmail.com>
2025-06-02 14:50:40 +0100

    create new Messages

---
+++ EventPlanner.Core/Messages/UpdateEventMessage.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using EventPlanner.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Core.Messages;
+
+public class UpdateEventMessage : ValueChangedMessage<string>
+{
+    public UpdateEventMessage(string value) : base(value)
+    {
+    }
+}
