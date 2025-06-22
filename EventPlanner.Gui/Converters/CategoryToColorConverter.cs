2e47e26b702c208c838c31a847a64ad52c8c84c4
weap0n3 <weap0n1823@gmail.com>
2025-06-22 21:27:24 +0100

    add new custom converter

---
+++ EventPlanner.Gui/Converters/CategoryToColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner.Gui.Converters;
+
+class CategoryToColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value == "Select Category" ? "#4E627E" : "#1F2937";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
